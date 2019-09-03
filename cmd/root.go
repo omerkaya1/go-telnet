6ed127b7d2cf00e977240848db6cb2badd93e63e
Omer Kaya <omerkaya513@gmail.com>
2019-09-03 16:08:05 +0300

    Initial commit.

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var clean bool
+
+var rootCmd = &cobra.Command{
+	Use:   "go-telnet [ADDRESS] [PORT]",
+	Short: "telnet replica utility written in Go",
+	Long:  "Runs any programme passed to goenvdir with a specified environment variables stored in a specified folder.",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return fmt.Errorf("invalid number of arguments")
+		}
+		return nil
+	},
+	Run: rootCommand,
+}
+
+// Execute is a method that runs the root command of the programme
+func Execute() {
+	rootCmd.PersistentFlags().BoolVarP(&clean, "clean", "c", false, "run programme with the empty environment variables")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func rootCommand(cmd *cobra.Command, args []string) {
+	pr := internal.NewProgRunnerImpl()
+	pr.ClearEnv = clean
+	pr.EnvPath = args[0]
+	pr.ChildProg = args[1]
+	if err := pr.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ go-telnet.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/omerkaya1/go-telnet/cmd"
+
+func main() {
+	cmd.Execute()
+}
